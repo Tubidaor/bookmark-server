3e9497faec8c3a29ac294a0b2862130f95ccb307
Juan Baltazar <juan.baltazar1@gmail.com>
2020-01-01 09:36:58 -0800

    added delete and post endpoints and tests with sterilization

--- src/bookmarks/bookmark-router.js
+++ src/bookmarks/bookmark-router.js
@@ -5,6 +5,15 @@ const uuid = require('uuid/v4')
 const logger = require('../logger')
 // const { bookmarks } = require('../store')
 const BookmarksService = require('../bookmarks-service')
+const xss = require('xss')
+
+const sterilizeBm = bookmark => ({
+  id: bookmark.id,
+  title: xss(bookmark.title),
+  url: bookmark.url,
+  description: xss(bookmark.description),
+  rating: bookmark.rating
+})
 
 
 
@@ -21,14 +30,15 @@ bookRouter
     const knexInstance = req.app.get('db')
     BookmarksService.getAllBookmarks(knexInstance)
       .then(bookmarks => {
-        res.json(bookmarks)
+        res.json(bookmarks.map(sterilizeBm))
     })
     .catch(next)
   })
   .post(jsonParser, (req, res, next) => {
     const { title, url, description, rating } = req.body
     const newBookmark = { title, url, description, rating }
-    for (const [key, value] of Object.entries(newBookmark)) {
+    const requiredFields = {title, url, rating }
+    for (const [key, value] of Object.entries(requiredFields)) {
       if (value == null) {
         return res.status(400).json({
           error: { message: `Missing '${key}' in request body` }
@@ -43,7 +53,7 @@ bookRouter
         res
           .status(201)
           .location(`/bookmarks/${bookmark.id}`)
-          .json(bookmark)
+          .json(sterilizeBm(bookmark))
       })
       .catch(next)
   })
@@ -51,51 +61,35 @@ bookRouter
 
   bookRouter
     .route('/bookmarks/:id')
-    .get((req, res) => {
-
-      const  { id } = req.params
-      // bookmarkSearch = bookmarks.find(bm => bm.id === id)
-      const knexInstance = req.app.get('db')
-      
-      // if(!bookmarkSearch) {
-        
-      //   return  res
-      //   .status(404)
-      //   .send("bookmark not found")
-      // }
-
-      BookmarksService.getById(knexInstance, id)
-        .then(BmById => {
-          if(!BmById) {
-            return res.status(404).json({
-              error: { message: `Bookmark doesn't exist` }
-            })
-          }
-          res.json(BmById)
-        })
-
-      // res.json(bookmarkSearch)
+    .all((req, res, next) => {
+      BookmarksService.getById(
+        req.app.get('db'),
+        req.params.id
+      )
+      .then(bookmark => {
+        if (!bookmark) {
+          return res.status(404).json({
+            error: { message: `Bookmark doesn't exist` }
+          })
+        }
+        res.bookmark = bookmark
+        next()
+      })
+      .catch(next)
     })
-    .delete((req, res) => {
-      const { id } = req.params;
-  
-      const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
-    
-      if ( bookmarkIndex === -1 ) {
-        logger.error(`Bookmark with id ${id} not found.`);
-        return res
-          .status(404)
-          .send('Not found');
-      }
-    
-      bookmarks.splice(bookmarkIndex, 1);
-    
-      logger.info(`Card with id ${id} deleted.`);
-    
-      res
-        .status(204)
-        .end();
+    .get((req, res, next) => {
+      res.json(sterilizeBm(res.bookmark))
     })
+    .delete((req, res, next) => {
+      BookmarksService.deleteBookmark(
+        req.app.get('db'),
+        req.params.id
+      )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
   module.exports = bookRouter
   
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -41,7 +41,6 @@ describe.only('Bookmark Endpoints', function() {
   context('Given there are bookmarks in the database', () => {
     const testBookmarks = makeBookmarksArray()
       
-    
     beforeEach('insert bookmarks', () => {
       return db
         .into('bookmarks')
@@ -64,15 +63,43 @@ describe.only('Bookmark Endpoints', function() {
 
   })
 
-  describe.only(`POST /bookmarks`, () => {
+  context(`Given an XSS attack bookmark`, () => {
+    const maliciousBookmark = {
+    id: 911,
+    title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    url: 'www.howto.com',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    rating: 3
+    }
+    
+    beforeEach('insert malicious bookmark', () => {
+      return db
+        .into('bookmarks')
+        .insert([ maliciousBookmark ])
+      })
+    
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/bookmarks/${maliciousBookmark.id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
+            expect(res.body.description).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`)
+          })
+      })
+  })
+
+
+  describe(`POST /bookmarks`, () => {
     it(`creates a bookmark, responding with 201 and the new bookmark`, function() {
       this.retries(3)
       const newBookmark = {
-        title: 'Reedit',
+        title: 'Reddit',
         url: 'www.reddit.com',
         description: 'Test new article content...',
         rating: 2
       }
+      const ratingValue = [1, 2, 3, 4, 5]
       return supertest(app)
         .post('/bookmarks')
         .send(newBookmark)
@@ -82,6 +109,8 @@ describe.only('Bookmark Endpoints', function() {
           expect(res.body.url).to.eql(newBookmark.url)
           expect(res.body.rating).to.eql(newBookmark.rating)
           expect(res.body).to.have.property('id')
+          expect(res.body.rating).to.be.a('number')
+          expect(ratingValue).to.include(res.body.rating)
           expect(res.headers.location).to.eql(`/bookmarks/${res.body.id}`)
         })
         .then(res =>
@@ -89,6 +118,61 @@ describe.only('Bookmark Endpoints', function() {
             .get(`/bookmarks/${res.body.id}`)
             .expect(res.body)
         )
+    })
+
+    const requiredFields = ['title', 'url', 'rating']
+    
+    requiredFields.forEach(field => {
+      const newBookmark = {
+        title: 'Test new bookmark',
+        url: 'wwww.newbookmark.com',
+        rating: 3
+      }
+    
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newBookmark[field]
+    
+        return supertest(app)
+          .post('/bookmarks')
+          .send(newBookmark)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          })
+      })
+    })
+  })
+
+  describe(`DELETE /bookmarkss/:id`, () => {
+    context(`Given no bookmarks`, () => {
+      it(`responds with 404`, () => {
+        const BmId = 123456
+        return supertest(app)
+          .delete(`/bookmarks/${BmId}`)
+          .expect(404, { error: { message: `Bookmark doesn't exist` } })
+      })
+    })
+
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = makeBookmarksArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      })
+
+      it('responds with 204 and removes the article', () => {
+        const idToRemove = 2
+        const expectedBookmarks = testBookmarks.filter(bookmark => bookmark.id !== idToRemove)
+        return supertest(app)
+          .delete(`/bookmarks/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/bookmarks`)
+              .expect(expectedBookmarks)
+          )
+      })
     })
   })
 })
